a6992b7fe344f3bad7fa0408282747c7f0bf42eb
Lucas Paixão <146206405+LukePassion@users.noreply.github.com>
2025-10-02 15:34:41 -0300

    SuperTrunfo_Iniciante.c
    Código para gerar duas cartas de Super Trunfo através da leitura de dados fornecidos pelo usuário.

--- SuperTrunfo_Iniciante.c
+++ SuperTrunfo_Iniciante.c
@@ -30,10 +30,6 @@ int main() {
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos1);
 
-    // Calculando os indicadores da Carta 1
-    densidade1 = (float)populacao1 / area1;
-    pib_per_capita1 = (pib1 * 1000000000) / populacao1;
-
     // Leitura dos dados da Carta 2
     printf("\nDigite os dados da Carta 2:\n");
     printf("Estado (A-H): ");
@@ -51,10 +47,6 @@ int main() {
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos2);
 
-    // Calculando os indicadores da Carta 2
-    densidade2 = (float)populacao2 / area2;
-    pib_per_capita2 = (pib2 * 1000000000) / populacao2;
-
     // Exibindo as cartas
     printf("\n Carta 1 \n");
     printf("Estado: %c\n", estado1);
@@ -64,8 +56,6 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Pontos Turísticos: %d\n", pontos_turisticos1);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
-    printf("PIB per capita: %.2f reais\n", pib_per_capita1);
 
     printf("\n Carta 2 \n");
     printf("Estado: %c\n", estado2);
@@ -75,8 +65,6 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Pontos Turísticos: %d\n", pontos_turisticos2);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
-    printf("PIB per capita: %.2f reais\n", pib_per_capita2);
 
     return 0;
 }
